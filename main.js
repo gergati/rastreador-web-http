9bb66133fab59800d96fc4de21c91b3782d7a77a
germangatica <gergati1994@gmail.com>
2024-09-28 19:58:55 -0300

    printReport finished

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { printReport } = require('./report.js')
 const { crawlPage } = require('./crawl.js')
 
 async function main() {
@@ -12,9 +13,7 @@ async function main() {
     const baseURL = process.argv[2]
     console.log(`Starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-    for (const page of Object.entries(pages)) {
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 main()
