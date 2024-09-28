a8653d2c57342c30816d8253dff266627f1a6691
germangatica <gergati1994@gmail.com>
2024-09-28 18:05:16 -0300

    getURLFromHTML function and test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            //Relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with relative url: ${error.message}`)
+            }
+        } else {
+            //Absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with absolute url: ${error.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
@@ -9,5 +37,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
